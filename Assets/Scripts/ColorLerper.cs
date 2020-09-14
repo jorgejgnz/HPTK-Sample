f02f999a8b9a6772481554fa2d9530b32d2894e0
Jorge J Gnz <jorge.jgnz94@gmail.com>
2020-09-14 06:39:08 +0200

    Script moved. Cuteness improved

---
+++ Assets/Scripts/ColorLerper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorLerper : MonoBehaviour
+{
+    public SkinnedMeshRenderer smr;
+
+    public Color mincolor;
+    public Color maxColor;
+    public string shaderColorParamName;
+
+    public void UpdateColor(float lerp)
+    {
+        smr.material.SetColor(shaderColorParamName,Color.Lerp(mincolor,maxColor,lerp));
+    }
+}
---
+++ Assets/Scripts/ProxyHandLerpEvents.cs
@@ -0,0 +1,22 @@
+﻿using HPTK.Views.Events;
+using HPTK.Views.Handlers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ProxyHandLerpEvents : MonoBehaviour
+{
+    public ProxyHandHandler proxyHand;
+
+    public FloatEvent onGraspLerpUpdate;
+    public FloatEvent onIndexPinchLerpUpdate;
+    public FloatEvent onErrorLerpUpdate;
+
+    private void Update()
+    {
+        onGraspLerpUpdate.Invoke(proxyHand.viewModel.graspLerp);
+        onIndexPinchLerpUpdate.Invoke(proxyHand.viewModel.indexPinchLerp);
+        onErrorLerpUpdate.Invoke(proxyHand.viewModel.errorLerp);
+    }
+}
