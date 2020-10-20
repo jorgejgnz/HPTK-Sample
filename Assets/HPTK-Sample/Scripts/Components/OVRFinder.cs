2a6dc763d71526949c080e2c610e74cd5433c4a4
Jorge J Gnz <jorge.jgnz94@gmail.com>
2020-10-20 03:59:25 +0200

    OVRFinder option to copy material from default SMR to slave on Start

--- Assets/HPTK-Sample/Scripts/Components/OVRFinder.cs
+++ Assets/HPTK-Sample/Scripts/Components/OVRFinder.cs
@@ -1,12 +1,17 @@
 ﻿using HPTK;
 using HPTK.Helpers;
 using HPTK.Input;
+using HPTK.Models.Avatar;
+using HPTK.Views.Handlers;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class OVRFinder : MonoBehaviour
 {
+    public AvatarHandler avatar;
+
     [Range(0.0f,5.0f)]
     public float waitFor = 3.0f;
 
@@ -15,9 +20,12 @@ public class OVRFinder : MonoBehaviour
     public OVRSkeletonTracker ovrSkeletonTrackerL;
     public OVRSkeletonTracker ovrSkeletonTrackerR;
 
+    [Header("HPTK Slave SMR")]
+    public bool copyDefaultSMRMaterial = false;
+
     [Header("Default SMR")]
     public bool disableDefaultSMR = true;
-    public bool applyMatToDefaultSMR = true;
+    public bool applyMatToDefaultSMR = false;
     public Material matToApply;
 
     [Header("Parent")]
@@ -27,6 +35,14 @@ public class OVRFinder : MonoBehaviour
     [Header("Layer")]
     public bool applyLayerUnderRoot = true;
     public string layerToApply = "HPTK";
+
+    [Header("Events")]
+    public UnityEvent onReady;
+
+    // Private
+    private bool ready = false;
+    SkinnedMeshRenderer defaultSMR_L;
+    SkinnedMeshRenderer defaultSMR_R;
 
     private void Start()
     {
@@ -60,9 +76,6 @@ public class OVRFinder : MonoBehaviour
             if (disableDefaultSMR)
                 smr.enabled = false;
 
-            if (applyMatToDefaultSMR)
-                smr.material = matToApply;
-
             OVRSkeleton ovrSkelton = ovrHand.GetComponent<OVRSkeleton>();
             if (ovrSkelton == null)
             {
@@ -80,22 +93,46 @@ public class OVRFinder : MonoBehaviour
                 switch (skeltonType)
                 {
                     case OVRSkeleton.SkeletonType.HandLeft:
+
                         if (!ovrSkeletonTrackerL.handData || !ovrSkeletonTrackerL.boneData)
                         {
                             ovrSkeletonTrackerL.handData = ovrHand;
                             ovrSkeletonTrackerL.boneData = ovrSkelton;
                         }
+
+                        defaultSMR_L = smr;
+
+                        // Copy materials from origin
+                        if (copyDefaultSMRMaterial)
+                            avatar.viewModel.leftHand.viewModel.slave.skinnedMR.material = defaultSMR_L.material;
+
                         break;
+
                     case OVRSkeleton.SkeletonType.HandRight:
+
                         if (!ovrSkeletonTrackerR.handData || !ovrSkeletonTrackerR.boneData)
                         {
                             ovrSkeletonTrackerR.handData = ovrHand;
                             ovrSkeletonTrackerR.boneData = ovrSkelton;
                         }
+
+                        defaultSMR_R = smr;
+
+                        // Copy materials from origin
+                        if (copyDefaultSMRMaterial)
+                            avatar.viewModel.rightHand.viewModel.slave.skinnedMR.material = defaultSMR_R.material;
+
                         break;
                 }
             }
+
+            // Apply materials to origin
+            if (applyMatToDefaultSMR)
+                smr.material = matToApply;
         }
+
+        ready = true;
+        onReady.Invoke();
     }
 
     public static void ApplyLayerRecursively(Transform root, int layer)
